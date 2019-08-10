aa0b540648d0d86cedd93b897639f6b9767e2b42
Nishikokura Hironobu <nishiko@mindia.jp>
2019-08-10 13:52:38 +0000

    set verbose_name to Workload

--- workloads/models.py
+++ workloads/models.py
@@ -3,11 +3,11 @@ from django.utils import timezone
 
 class Workload(models.Model):
     is_done = models.BooleanField(
-        verbose_name='',
+        verbose_name='is_done',
         default=False,
     )
     number = models.IntegerField(
-        verbose_name='',
+        verbose_name='number',
         blank=True,
         null=True,
         default=1,
@@ -15,13 +15,13 @@ class Workload(models.Model):
         #             validators.MaxValueValidator(100)]
     )
     created_at = models.DateTimeField(
-        verbose_name='',
+        verbose_name='created_at',
         blank=True,
         null=True,
         default=timezone.now
     )
     updated_at = models.DateTimeField(
-        verbose_name='',
+        verbose_name='updated_at',
         blank=True,
         null=True,
         default=timezone.now
