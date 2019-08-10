1fc66195c11f5a972f7832a670ac1a91bde8e605
Nishikokura Hironobu <nishiko@mindia.jp>
2019-08-10 07:27:54 +0000

    python manage.py startapp workloads

--- nishikocloud/settings.py
+++ nishikocloud/settings.py
@@ -75,8 +75,9 @@ WSGI_APPLICATION = 'nishikocloud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nishikocloud',
+        'USER': 'root'
     }
 }
 
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Japan'
 
 USE_I18N = True
 
