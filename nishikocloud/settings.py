fa2e6ffde576d9d4eac2635ee0fd3ce3fbe89ef2
Nishikokura Hironobu <nishiko@mindia.jp>
2019-08-11 10:59:00 +0000

    use local_settings.py

--- nishikocloud/settings.py
+++ nishikocloud/settings.py
@@ -15,17 +15,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3nzok6sxmxyz7qerr0e5reho1l+%_6rd%l#x=v8x53kh-e*lma'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -127,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
