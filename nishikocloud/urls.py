5b0148a4617386d26a539a947c999dec0820c014
Nishikokura Hironobu <nishiko@mindia.jp>
2019-08-12 00:28:27 +0000

    add a view of workloads#index

--- nishikocloud/urls.py
+++ nishikocloud/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('workloads.urls')),
     path('workloads/', include('workloads.urls')),
     path('admin/', admin.site.urls),
 ]
--- workloads/views.py
+++ workloads/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Workload
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the 245cloud !")
+    latest_workload_list = Workload.objects.order_by('-created_at')[:5]
+    template = loader.get_template('workloads/index.html')
+    context = {
+        'latest_workload_list': latest_workload_list,
+    }
+    return HttpResponse(template.render(context, request))
